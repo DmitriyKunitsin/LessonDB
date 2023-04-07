9af76097ec93e8b3709a05169b09f6df228a695a
kunicind1996@gmail.com <kunicind1996@gmail.com>
2023-04-07 22:19:24 +0700

    Установил базу данных

---
+++ AutoregWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LessonDB
+{
+    /// <summary>
+    /// Логика взаимодействия для AutoregWindow.xaml
+    /// </summary>
+    public partial class AutoregWindow : Window
+    {
+        public AutoregWindow()
+        {
+            InitializeComponent();
+        }
+        string login = TextBoxLogin.Text.Trim();
+        string pass = TextBoxPassword.Password.Trim();
+        string email = TextBoxEmail.Text.ToLower().Trim();
+
+        private void Button_Auto_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
