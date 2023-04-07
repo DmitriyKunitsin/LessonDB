95e8c88d287b8463aaf0fb8d9d4c027f5378d50e
kunicind1996@gmail.com <kunicind1996@gmail.com>
2023-04-07 12:51:55 +0700

    Добавьте файлы проекта.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,60 @@ namespace LessonDB
     /// </summary>
     public partial class MainWindow : Window
     {
+        ApplicationContext db;
         public MainWindow()
         {
             InitializeComponent();
+            
+            db= new ApplicationContext();
+
+          
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = TextBoxLogin.Text.Trim();
+            string pass = TextBoxPassword.Password.Trim();
+            string pass_2 = TextBoxPassword_2.Password.Trim();
+            string email = TextBoxEmail.Text.ToLower().Trim();
+
+            if (login.Length < 4)
+            {
+                TextBoxLogin.ToolTip = "Короткий логин"; // всплывающая подсказка
+                TextBoxLogin.Background = Brushes.Red; // задний фон
+
+            }
+            else if (pass.Length < 4)
+            {
+                TextBoxPassword.Background = Brushes.Red; // задний фон
+                TextBoxPassword.ToolTip = "Короткий пароль"; // всплывающая подсказка
+            }
+            else if (pass != pass_2)
+            {
+                MessageBox.Show("Пароли не совпадают");
+            }
+            else if (email.Length < 4 || !email.Contains("@") || !email.Contains("."))
+            {
+                TextBoxEmail.Background = Brushes.Red; // задний фон
+                TextBoxEmail.ToolTip = "Не корректная почта"; // всплывающая подсказка
+            }
+            else
+            {
+                TextBoxLogin.ToolTip = "";
+                TextBoxLogin.Background = Brushes.Transparent; // Прозрачный задний фон
+                TextBoxPassword.ToolTip = "";
+                TextBoxPassword.Background = Brushes.Transparent;
+                TextBoxPassword_2.ToolTip = "";
+                TextBoxPassword_2.Background = Brushes.Transparent;
+                TextBoxEmail.ToolTip = "";
+                TextBoxEmail.Background = Brushes.Transparent;
+                MessageBox.Show("Ожидайте подтверждения регистрации администратором");
+                User user = new User(login,email,pass);
+                
+                db.Users.Add(user);
+                db.SaveChanges();
+            }
+            }
         }
     }
-}
+
